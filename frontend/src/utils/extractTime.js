bd57e7e8fa766e32695aef32f67e457a90f41449
Naved Ali <naved10055@gmail.com>
2024-02-21 17:40:37 +0530

    extractTime function am pm

--- frontend/src/utils/extractTime.js
+++ frontend/src/utils/extractTime.js
@@ -2,7 +2,9 @@ export function extractTime(dateString) {
 	const date = new Date(dateString);
 	const hours = padZero(date.getHours());
 	const minutes = padZero(date.getMinutes());
-	return `${hours}:${minutes}`;
+	const twelveHourFormat = hours % 12 || 12;
+
+	return `${twelveHourFormat}:${minutes} ${period}`;
 }
 
 // Helper function to pad single-digit numbers with a leading zero
