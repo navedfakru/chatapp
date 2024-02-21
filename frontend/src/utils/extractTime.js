784798b015c98c12eb55db1643b97d5c18008129
Naved Ali <naved10055@gmail.com>
2024-02-21 17:46:01 +0530

    extractTime function am pm

--- frontend/src/utils/extractTime.js
+++ frontend/src/utils/extractTime.js
@@ -2,8 +2,8 @@ export function extractTime(dateString) {
 	const date = new Date(dateString);
 	const hours = padZero(date.getHours());
 	const minutes = padZero(date.getMinutes());
+	const period = hours >= 12 ? 'PM' : 'AM';
 	const twelveHourFormat = hours % 12 || 12;
-
 	return `${twelveHourFormat}:${minutes} ${period}`;
 }
 
